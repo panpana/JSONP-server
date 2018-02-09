3d0740616843fd477d9a9817e6661c455bf0e5ae
panpana <867159647@qq.com>
2018-02-10 02:42:05 +0800

    create

---
+++ server.js
@@ -0,0 +1,47 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
+  var method = request.method
+    if (path === '/') {
+        var string = fs.readFileSync('./index.html', 'utf8')
+        var amount = fs.readFileSync('./db', 'utf8')
+        string = string.replace("&&&amount&&&", amount)
+        response.setHeader('Content-Type', 'text/html;charset=utf-8')
+        response.write(string)
+        response.end()
+    } else if (path === '/pay') {
+        var amount = fs.readFileSync('./db', 'utf8')
+        var newAmount = amount - 1
+        fs.writeFileSync('./db', newAmount)
+        response.setHeader('Content-Type', 'application/javascript')
+        response.statusCode = 200
+        response.write(`${query.callbackName}.call(undefined,'success')`)
+        //response.write(`amount.innerText = amount.innerText - 1`)
+
+        response.end()
+
+    } else {
+        response.statusCode = 404
+        response.write('找不到路径')
+        response.end()
+    }
+
+
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
